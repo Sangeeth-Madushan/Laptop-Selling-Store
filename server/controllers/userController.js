5ff6b01d040d29a9cf72cb36fd91173b7361c4ee
sangeeth <sangeeth99614@gmail.com>
2025-05-12 11:55:53 +0530

    add User controller with registration and login logic

---
+++ server/controllers/userController.js
@@ -0,0 +1,127 @@
+import User from "../models/user.js";
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+
+
+export function getUser(req, res) {
+    User.find().then((data)=>{
+        res.json(data);
+
+    }).catch(() => {
+      res.json({
+        message: "users not found",
+      });
+    });
+}
+
+export function saveUser(req, res) {
+
+    if (req.user == null) {
+  res.status(401).json({
+    message: "You are not logged in. Please login.",
+  });
+  return;
+}
+
+if (req.user.role !== "admin") {
+  res.status(403).json({
+    message: "You are not authorized to create an admin because you are not an admin.",
+  });
+  return;
+}
+
+// If the user is logged in and is an admin, continue with the task...
+
+
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+
+  const user = new User({
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: hashedPassword,
+    role: req.body.role,
+
+  });
+
+  user.save().then(()=>{
+    res.json({
+        message: "user saved successfull"
+    })
+  }).catch(()=>{
+    res.json({
+        messsage: "user added failed",
+    })
+  })
+}
+
+export function loginUser(req,res){
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne( {email :email}).then(
+        (user)=>{
+            if(user == null){
+                res.json({
+                    message : "user not found"
+                })
+            }else{
+
+                //password entered by the user (password) matches the saved (hashed) password in the database (user.password).
+                const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+                if(isPasswordCorrect){
+
+                    const token = jwt.sign({
+                        email : user.email,
+                        firstName: user.firstName,
+                        lastName : user.lastName,
+                        role : user.role,
+                        img : user.img
+                    },
+                    "AbcJayalath99"
+                ) 
+
+                res.json({
+                    message:"passsword is correct you have access",
+                    token: token
+
+                })
+
+
+
+                }else{
+                    res.json({
+                        messsage : "invalid password"
+                    })
+                }
+            }
+        }
+
+
+
+    ).catch(()=>{
+        res.json({message: "something went wrong "})
+    })
+
+
+}
+
+
+// short code for checking user is admin or not
+export function isAdmin(req){
+
+   // check user imfor missing or user is not login
+  if (req.user == null) {
+    
+    return false; // stop here
+  }
+
+    // check user is not a admin 
+  if (req.user.role != "admin") {
+    
+    return false; // stop here too
+  }
+
+}
