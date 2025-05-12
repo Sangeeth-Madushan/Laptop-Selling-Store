2848f3bd14e09fc400289b06bf593aff614e3849
sangeeth <sangeeth99614@gmail.com>
2025-05-12 12:20:05 +0530

    add JWT authentication middleware to validate user tokens

--- server/index.js
+++ server/index.js
@@ -5,8 +5,36 @@ import express from "express";
 //testing commit
 const app = express();
 
-mongoose.connect("")
 app.use(bodyParser.json());
+app.use((req,res,next)=>{
+  const tokenString = req.header("Authorization");
+  if(tokenString != null){
+
+    const token = tokenString.replace("Bearer ", "")
+   
+    jwt.verify(token, "AbcJayalath99",(err, decoded)=>{
+     
+      if(decoded != null){
+        // Stores the decoded user data with user request inside req.user
+        req.user = decoded
+        next();
+      }else{
+        res.status(403).json({
+          message: "Invalid token",
+        });
+      }
+
+    })
+    // when user try to log into the system no token
+  }else{
+    next();
+  }
+
+
+});
+
+
+
 mongoose
   .connect(
     "mongodb+srv://user:12345@cluster0.mdhspzj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
