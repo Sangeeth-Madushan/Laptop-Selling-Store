bbb19807d073e6388abd6c6dd1460089ab7340a8
sangeeth <sangeeth99614@gmail.com>
2025-05-27 15:47:33 +0530

    Add Review model to store product reviews

---
+++ server/models/review.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+  reviewId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  productId: {
+    type: String,
+    required: true
+    
+  },
+  userEmail: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  comment: {
+    type: String,
+    required: true,
+    maxlength: 1000
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Review = mongoose.model("reviews", reviewSchema);
+export default Review;
