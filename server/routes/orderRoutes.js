02ee31fe0d3ffb13b6967cebe9345c1cd554107f
sangeeth <sangeeth99614@gmail.com>
2025-06-20 20:59:44 +0530

    Added update order function

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 
 import express from "express";
-import { createOrder, getOrders } from "../controllers/orderController.js";
+import { createOrder, getOrders, updateOrderStatus } from "../controllers/orderController.js";
 import verifyJWT from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 orderRouter.post("/", verifyJWT, createOrder); 
-orderRouter.get("/", verifyJWT, getOrders)
+orderRouter.get("/", verifyJWT, getOrders);
+orderRouter.put("/:orderId/:status",verifyJWT,updateOrderStatus);
 
 export default orderRouter;
