e6879158f1306d43b470a71766a8035f712bbb43
sangeeth <sangeeth99614@gmail.com>
2025-06-20 19:45:35 +0530

    Added get order function

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 
 import express from "express";
-import { createOrder } from "../controllers/orderController.js";
+import { createOrder, getOrders } from "../controllers/orderController.js";
 import verifyJWT from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 orderRouter.post("/", verifyJWT, createOrder); 
+orderRouter.get("/", verifyJWT, getOrders)
 
 export default orderRouter;
